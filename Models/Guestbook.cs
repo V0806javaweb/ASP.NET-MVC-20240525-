ea5dc9ee816978c57af9e027b3f91049b86f596b
柏維 陳 <tp61i6jo6168@gmail.com>
2024-04-12 22:08:40 +0800

    encode

---
+++ Models/Guestbook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MemberSystem.Models
+{
+    public class Guestbook
+    {
+        //編號
+        public int Id { get; set; }
+
+    }
+}
