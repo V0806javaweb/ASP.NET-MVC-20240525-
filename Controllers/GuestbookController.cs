9e21418705ce3867d2c182b3f57d389624378089
柏維 陳 <tp61i6jo6168@gmail.com>
2024-04-13 12:01:52 +0800

    顯示留言板

---
+++ Controllers/GuestbookController.cs
@@ -0,0 +1,27 @@
+﻿using MemberSystem.Services;
+using MemberSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemberSystem.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        // table Guestbooks service object
+        private readonly GuestbookDBService GuestbookService = new GuestbookDBService();
+
+        // GET: Guestbook
+        public ActionResult Index()
+        {
+            //page model
+            GuestbookViewModel Data = new GuestbookViewModel();
+            //get data from service
+            Data.DataList = GuestbookService.GetDataList();
+            //deliver Data to viewpage
+            return View(Data);
+        }
+    }
+}
